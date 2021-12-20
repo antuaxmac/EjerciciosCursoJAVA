27da1719126b7cfe7ac6dad87f76c626c0d8de6e
antuaxmac <96445398+antuaxmac@users.noreply.github.com>
2021-12-20 21:36:22 +0100

    Add files via upload

---
+++ src/com/exercices/Ejercicio01.java
@@ -0,0 +1,47 @@
+package com.exercices;
+
+public class Ejercicio01 {
+
+    public static void main(String[] args){
+
+        // datos primitivos
+
+             // numéricos enteros
+                byte variable01 = 2;
+                short variable02 = 5;
+                int variable03 = 10;
+                long variable04 = 547;
+
+            // numéricos decimales
+                float variable05 = 3.45f;
+                double variable06 = 5.25d;
+
+            // booleanos
+                boolean variable07 = true;
+                boolean variable08 = false;
+
+            // texto
+                char variable09 = 'a';
+
+        // datos NO primitivos
+
+            // texto
+                String variable10 = "dirección";
+
+
+                System.out.println(variable01 + " - Esto es un tipo de dato ´byte´, primitivo, numérico entero");
+                System.out.println(variable02 + " - Esto es un tipo de dato ´short´, primitivo, numérico entero");
+                System.out.println(variable03 + " - Esto es un tipo de dato ´int´, primitivo, numérico entero");
+                System.out.println(variable04 + " - Esto es un tipo de dato ´long´, primitivo, numérico entero");
+                System.out.println(variable05 + " - Esto es un tipo de dato ´float´, primitivo, numérico decimal");
+                System.out.println(variable06 + " - Esto es un tipo de dato ´double´, primitivo, numérico decimal");
+                System.out.println(variable07 + " - Esto es un tipo de dato ´booleano´, primitivo, booleano");
+                System.out.println(variable08 + " - Esto es un tipo de dato ´booleano´, primitivo, booleano");
+                System.out.println(variable09 + " - Esto es un tipo de dato ´texto´, primitivo, de carácter");
+                System.out.println(variable10 + " - Esto es un tipo de dato ´texto´, NO primitivo, de cadena de texto");
+
+                System.out.println("Ejercicio completado");
+
+
+    }
+}
