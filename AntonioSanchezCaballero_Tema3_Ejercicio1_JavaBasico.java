9480595d0180c53126fdd1cba21f85d6760e1558
antuaxmac <96445398+antuaxmac@users.noreply.github.com>
2021-12-29 22:16:54 +0100

    Add files via upload

---
+++ AntonioSanchezCaballero_Tema3_Ejercicio1_JavaBasico.java
@@ -0,0 +1,25 @@
+package com.exercices;
+
+public class Ejercicio03B {
+
+
+    public static void main(String[] args) {
+        /*
+            Creación de un bucle que permita concatenar textos
+            separados por espacios e imprima el resultado final
+            por consola
+
+         */
+
+        String[] nombres = {"Antonio", "Belén", "Carolina", "Martina"};
+
+        for (String nombre : nombres) {
+
+            System.out.println(nombres[0] + " " + nombres[1] + " " + nombres[2] + " " + nombres[3]);
+
+        }
+
+
+    }
+}
+
