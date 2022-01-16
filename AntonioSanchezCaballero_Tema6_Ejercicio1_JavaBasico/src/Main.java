9afc866befcbd46b34b384fcda4301396b1ad0a0
antuaxmac <96445398+antuaxmac@users.noreply.github.com>
2022-01-16 09:44:31 +0100

    Add files via upload

---
+++ AntonioSanchezCaballero_Tema6_Ejercicio1_JavaBasico/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+            public static void main(String[] args) {
+
+            // Ver los catálogos de técnicas de refactorización:
+
+            // https://refactoring.com/catalog/
+
+            // https://refactoring.guru/refactoring/catalog
+        }
+    }
